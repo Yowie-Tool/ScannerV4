3d89b5619842f8e0e9cfadce4fb3a4406ec9f950
LettyYeti <lettie.adkins@yetitool.com>
2020-10-13 20:58:08 +0100

    debuggin

--- comms/serial_connection.py
+++ comms/serial_connection.py
@@ -120,7 +120,7 @@ class SerialConnection(object):
 		if self.s:
 			try:
 				self.s.write((serialCommand).encode('utf-8')) # assume everything needs encoding??
-
+				log('write ' + serialCommand + ' to serial')
 			except:
 				print("FAILED to write to SERIAL: " + serialCommand + " (Alt text: " + str(altDisplayText) + ")")
 
