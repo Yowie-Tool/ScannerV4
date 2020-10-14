53bb5928eccdc67ae971f9f764ca8c22c132cbcf
LettyYeti <lettie.adkins@yetitool.com>
2020-10-14 22:37:13 +0100

    hack camera steps

--- comms/scanner_machine.py
+++ comms/scanner_machine.py
@@ -421,10 +421,7 @@ class ScannerMachine(object):
 			#There is a way of taking photo's directly into opencv as an array, but previous attempts at this have been unsuccesful, it seems this only works at low resolutions.
 			loff=cv.imread(loffname)
 			lon=cv.imread(lonname)
-			print(loff.shape)
-			print(lon.shape)
 			src=cv.subtract(lon,loff)
-			print(src.shape)
 			#subtract the laser on image from the laser off image. In theory, when we lock down the camera settings between the two photos, we should end up with just the laser line left. In practice, there is extra interference involved. 
 			blue=src[:,:,0]
 			#extract just the blue array, as this is the main proportion of the IR laser image
