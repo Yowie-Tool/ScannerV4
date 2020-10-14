6855ac1b9017904698dc328303f955fc0ff0256b
LettyYeti <lettie.adkins@yetitool.com>
2020-10-14 15:50:03 +0100

    added welcome screen to start serial loop

--- screens/screen_welcome.py
+++ screens/screen_welcome.py
@@ -53,6 +53,7 @@ class WelcomeScreenClass(Screen):
         
         super(WelcomeScreenClass, self).__init__()
         self.sm=kwargs['screen_manager']
+        self.name = kwargs['name']
         self.m=kwargs['scanner']
 
     def on_enter(self):
