d312020f38e8e9e065899ebc245f37e7d9123077
Adrian Bowyer <adrian@reprappro.com>
2021-09-29 14:33:56 +0100

    James's mods.

--- screens/screen4.py
+++ screens/screen4.py
@@ -3,20 +3,15 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
-
 from comms import usb_storage
-
 Builder.load_string("""
-
 <YowieScreen4>:
-
 	canvas:
-		Color: 
+		Color:
 			rgba: [1, 1, 1, 1]
-		Rectangle: 
+		Rectangle:
 			size: self.size
 			pos: self.pos
-
 	BoxLayout:
 		orientation: 'horizontal'
 		padding: 0
@@ -24,19 +19,16 @@ Builder.load_string("""
 		size_hint: (None, None)
 		height: dp(480)
 		width: dp(800)
-
 		# SCREEN BUTTONS
 		BoxLayout:
 			orientation: 'horizontal'
 			size_hint: (None, None)
 			height: dp(480)
 			width: dp(180)
-
 			GridLayout:
 				size: self.parent.size
 				pos: self.parent.pos
 				cols: 2
-
 				# Top right
 				Button:
 					on_press: root.go_s2()
@@ -52,7 +44,6 @@ Builder.load_string("""
 						canvas.after:
 							PopMatrix
 						text: 'Scan Settings'
-
 				# Bottom right
 				Button:
 					on_press: root.go_s4()
@@ -68,7 +59,6 @@ Builder.load_string("""
 						canvas.after:
 							PopMatrix
 						text: 'Scan Output'
-
 				# Top left
 				Button:
 					on_press: root.go_s1()
@@ -84,7 +74,6 @@ Builder.load_string("""
 						canvas.after:
 							PopMatrix
 						text: 'Levelling'
-
 				# Bottom left
 				Button:
 					on_press: root.go_s3()
@@ -100,14 +89,11 @@ Builder.load_string("""
 						canvas.after:
 							PopMatrix
 						text: 'Scan'
-
 		BoxLayout:
 			orientation: 'horizontal'
 			size_hint: (None, None)
 			height: dp(480)
 			width: dp(200)
-
-
 		Button:
 			size_hint: (None, None)
 			height: dp(480)
@@ -128,97 +114,83 @@ Builder.load_string("""
 				markup: True
 				font_size: '22sp'
 				text: "Save"
-
-
-
-
 """)
-
 class YowieScreen4(Screen):
-
 	def __init__(self, **kwargs):
-
 		self.sm = kwargs['screen_manager']
 		self.name = kwargs['name']
 		self.m = kwargs['scanner']
-
 		super(YowieScreen4, self).__init__()
-
 		# self.usb_stick = usb_storage.USB_storage(self.sm)
 		# self.usb_stick.enable()
-
 # SCREEN BUTTONS
 	def go_s1(self):
 		self.sm.current = 's1'
-
 	def go_s2(self):
 		self.sm.current = 's2'
-
 	def go_s3(self):
 		self.sm.current = 's3'
-
 	def go_s4(self):
 		self.sm.current = 's4'
-
-
 # SAVE OUTPUT
-
 	def save_output(self):
-
 		file_path = '/media/pi/SCANFILES/' + "RoomReaderScan.pts"
 		file_pathAB = '/media/pi/SCANFILES/' + "RoomReaderScanAB.pts"
-
+		file_pathDebug = '/media/pi/SCANFILES/' + 'RoomReaderScanDebug.txt'
 		# if self.usb_stick.is_usb_mounted_flag == True:
-
 		# 	file_path = self.usb_stick.get_path() + "RoomReaderScan.pts"
-		# 	#currently only one file name. 
-
+		# 	#currently only one file name.
 		try:
 			file_object = open(file_path, "w")
-
-
 			print("Saving points file")
-			exportint = len(self.m.output) 
-
+			exportint = len(self.m.output)
 			for export in range(exportint):
-
 				print(export)
-
 				#I've deleted Z and RGB from this now. exports just a text file with X,Y coordinates
 				xout = str(self.m.output[export][0])
 				yout = str(self.m.output[export][1])
 				zout = str(self.m.output[export][2])
-
-# 				xout = str(self.m.output[export,0])
-# 				yout = str(self.m.output[export,1])
-# 				zout = str(self.m.output[export,2])
 				file_object.write(xout + " " + yout + " " + zout + "\n")
-
 			file_object.close()
 			file_objectAB = open(file_pathAB, "w")
 			
-			exportint = len(self.m.outputAB) 
-
+			exportint = len(self.m.outputAB)
 			for export in range(exportint):
-
 				print(export)
-
+				
 				#I've deleted Z and RGB from this now. exports just a text file with X,Y coordinates
-				xout = str(self.m.output[exportAB][0])
-				yout = str(self.m.output[exportAB][1])
-				zout = str(self.m.output[exportAB][2])
-
-# 				xout = str(self.m.output[export,0])
-# 				yout = str(self.m.output[export,1])
-# 				zout = str(self.m.output[export,2])
+				xout = str(self.m.outputAB[export][0])
+				yout = str(self.m.outputAB[export][1])
+				zout = str(self.m.outputAB[export][2])
 				file_objectAB.write(xout + " " + yout + " " + zout + "\n")
-
 			file_objectAB.close()
-
+			
+			file_objectDebug = open(file_pathDebug, "w")
+			
+			exportint = len(self.m.for_calc_1)
+			for export in range(exportint):
+				print(export)
+				#I've deleted Z and RGB from this now. exports just a text file with X,Y coordinates
+				xout = str(self.m.for_calc_1[export][0])
+				yout = str(self.m.for_calc_1[export][1])
+				rout = str(self.m.for_calc_1[export][2])
+				file_objectDebug.write(xout + " " + yout + " " + rout + "camera1\n")
+				
+			exportint = len(self.m.for_calc_2)
+			for export in range(exportint):
+				print(export)
+				
+				#I've deleted Z and RGB from this now. exports just a text file with X,Y coordinates
+				xout = str(self.m.for_calc_2[export][0])
+				yout = str(self.m.for_calc_2[export][1])
+				rout = str(self.m.for_calc_2[export][2])
+				file_objectDebug.write(xout + " " + yout + " " + rout + "camera2\n")				
+			file_objectDebug.close()
+			
 			# self.usb_stick.disable()
-
-		except: 
+		except:
 			print('Could not save file')
+
 
 
 
